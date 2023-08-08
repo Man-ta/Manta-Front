9d7e879394e1daf3199523a2230df2381e8edc47
jhuee <bse07210@gmail.com>
2023-08-09 02:09:15 +0900

    RouteAPI(Transit)

--- screens/TransitRoute.tsx
+++ screens/TransitRoute.tsx
@@ -3,7 +3,6 @@ import { SubwayModal } from "./modal/SubwayModal";
 import { SubwayDetail } from "./modal/SubwayDetail";
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import { DatePicker } from './modal/Picker'
 
 const TransitRoute = () => {
 
@@ -12,40 +11,32 @@ const TransitRoute = () => {
 
   const handleApiCall = () => {
     // API 호출을 위한 파라미터 설정 (TrainResponseDto 객체와 유사한 형식으로 설정)
-    const trainResponseDto = {
-      stationCode : 'P555',
-      dow: 'TUE',
-      hh: '10',
-      mm: '20'
-    };
-
-    // API 호출 URL과 API 키 설정 (실제 값으로 수정)
-    const apiUrl = 'http://43.201.115.180:8085/trains/congestion';
-    const appKey = 'Glus98D8701NAVDh5d0iB7BRUTtA7NX77DbSioES';
-
-    // API 호출
-    axios.get(apiUrl, {
-      params: trainResponseDto,
-      headers: {
-        appkey: 'Glus98D8701NAVDh5d0iB7BRUTtA7NX77DbSioES',
-        'accept' : 'application/json',
-        'Content-Type': 'application/json',
-
+    
+    axios({
+      method: 'POST',
+      url: 'http://192.168.35.57:8085/transit/route',
+      data: {
+        startX: "127.02550910860451",
+        startY: "37.63788539420793",
+        endX: "127.030406594109",
+        endY: "37.609094989686",
+        searchDttm: "202308081750"
       },
-    })
-    .then(response => {
-      // API 응답 결과를 상태에 저장
-      setApiResponse(response.data);
-      console.log(response.data);
-    })
-    .catch(error => {
-      console.error('API 호출 에러:', error);
-    });
-  };
+      headers: { 
+                  'Content-Type': 'application/json'
+                }
+    }).then(function (response) { //post 성공 시
+              console.log("Heade With Authentication :" + response)
+              console.log(response.data)
+              setApiResponse(response.data);
+            }
+    )
+
+}
   useEffect(() => {
     handleApiCall();
   }, []);
-  
+
 
   return (
     <>
